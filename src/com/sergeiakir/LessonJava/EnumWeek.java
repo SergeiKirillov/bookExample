f3ead529a3b8dec5fbbe77e224b6d137897f67ca
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-12-29 22:01:12 +0600

    график 07

--- src/com/sergeiakir/LessonJava/EnumWeek.java
+++ src/com/sergeiakir/LessonJava/EnumWeek.java
@@ -48,6 +48,45 @@ enum week5br{ Понедельник(1,false),Вторник(2,false),Среда
     }
 }
 
+enum Grafic07{
+    Понедельник(1){
+        @Override
+        String getHoliDay() {
+            return "1 бригада - рабочий, 2 бригада - выходной";
+        }
+    }, Вторник(2),Среда(3),Четверг(4),
+    Пятница(5){
+        @Override
+        String getHoliDay(){
+            return "1 бригада - выходной, 2 бригада - рабочий";
+        }
+    },
+    Суббота(6){
+        @Override
+        String getHoliDay(){
+            return "1 бригада - выходной, 2 бригада - рабочий";
+        }
+    },Воскресенье(7){
+        @Override
+        String getHoliDay() {
+            return "1 бригада - рабочий, 2 бригада - выходной";
+        }
+    };
+
+    private int NumberDay;
+    private int Brig;
+
+    Grafic07(int numberDay){
+        this.NumberDay=numberDay;
+
+    }
+    String getHoliDay() {
+        return "1 бригада - рабочий, 2 бригада - рабочий";
+    }
+}
+
+
+
 
 public class EnumWeek {
 
@@ -59,6 +98,17 @@ public class EnumWeek {
         System.out.println(work.toString());
         System.out.println(work.getHoliday());
         System.out.println(work.getNameDay());
+
+
+        for (Grafic07 E: Grafic07.values()){
+            //Grafic07 br1 = Grafic07.;
+            System.out.println(E.toString());
+            System.out.println(E.getHoliDay());
+        }
+
+
+
+
     }
 
 
